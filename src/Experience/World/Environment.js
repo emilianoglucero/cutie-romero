46f634be35ca9cd0952f6bdbaa1a2b03a0139ec2
emiliano lucero <emilianoglucero@gmail.com>
2023-10-04 19:33:31 -0300

    sun light removed

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -7,32 +7,7 @@ export default class Environment {
 
     this.debug = this.experience.experience.debug;
 
-    // this.setSunLight();
     this.ambientLight();
-  }
-
-  setSunLight() {
-    this.sunLight = new THREE.DirectionalLight(0xffffff, 4);
-    this.sunLight.castShadow = true;
-    this.sunLight.shadow.camera.far = 15;
-    this.sunLight.shadow.mapSize.set(1024, 1024);
-    this.sunLight.shadow.normalBias = 0.05;
-    this.sunLight.position.set(3, 3, -2.25);
-    this.scene.add(this.sunLight);
-
-    //helper for sun light
-    this.sunLightHelper = new THREE.DirectionalLightHelper(this.sunLight, 1);
-    console.log(this.sunLightHelper);
-    this.scene.add(this.sunLightHelper);
-
-    //my code
-    // this.hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 5);
-    // this.hemiLight.position.set(0, 200, 0);
-    // this.scene.add(this.hemiLight);
-    // //helper for hemi light
-    // this.hemiLightHelper = new THREE.HemisphereLightHelper(this.hemiLight, 10);
-    // console.log(this.hemiLightHelper);
-    // this.scene.add(this.hemiLightHelper);
   }
 
   ambientLight() {
