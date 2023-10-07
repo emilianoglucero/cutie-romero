bacfa9aee7031a18f93056cfbb41ebde72e1263c
emiliano lucero <emilianoglucero@gmail.com>
2023-10-07 15:13:05 -0300

    -lod level detail added to videos -matrix auto update set to false

--- src/Experience/World/CutiPlaneImagesTitle.js
+++ src/Experience/World/CutiPlaneImagesTitle.js
@@ -13,8 +13,11 @@ export default class CutiPlaneImagesTitle {
   }
 
   setGeometry() {
+    this.lod = new THREE.LOD();
     this.geometry1 = new THREE.PlaneGeometry(1, 1, 1, 1);
     this.geometry2 = new THREE.PlaneGeometry(2, 2, 100, 100);
+    this.geometry2LowDetails = new THREE.PlaneGeometry(2, 2, 25, 25);
+    this.geometry2MediumDetails = new THREE.PlaneGeometry(2, 2, 35, 35);
   }
 
   setTextures() {
@@ -62,23 +65,53 @@ export default class CutiPlaneImagesTitle {
 
   setMesh() {
     this.mesh1 = new THREE.Mesh(this.geometry2, this.cutiMaterial1);
+    this.mesh1MediumDetails = new THREE.Mesh(
+      this.geometry2MediumDetails,
+      this.cutiMaterial1
+    );
+    this.mesh1LowDetails = new THREE.Mesh(
+      this.geometry2LowDetails,
+      this.cutiMaterial1
+    );
+    this.mesh1.matrixAutoUpdate = false;
     this.mesh1.position.set(-0.43, 0.66, 2.88);
     this.mesh1.scale.set(1.35, 1.35, -7.3);
     this.mesh1.rotation.set(0, 0.66, 0.1);
+    this.mesh1.updateMatrix();
+    this.lod.addLevel(this.mesh1, 0);
+
+    this.mesh1MediumDetails.matrixAutoUpdate = false;
+    this.mesh1MediumDetails.position.set(-0.43, 0.66, 2.88);
+    this.mesh1MediumDetails.scale.set(1.35, 1.35, -7.3);
+    this.mesh1MediumDetails.rotation.set(0, 0.66, 0.1);
+    this.mesh1MediumDetails.updateMatrix();
+    this.lod.addLevel(this.mesh1MediumDetails, 10);
+
+    this.mesh1LowDetails.matrixAutoUpdate = false;
+    this.mesh1LowDetails.position.set(-0.43, 0.66, 2.88);
+    this.mesh1LowDetails.scale.set(1.35, 1.35, -7.3);
+    this.mesh1LowDetails.rotation.set(0, 0.66, 0.1);
+    this.mesh1LowDetails.updateMatrix();
+    this.lod.addLevel(this.mesh1LowDetails, 20);
 
     this.flower1 = new THREE.Mesh(this.geometry1, this.cutiMaterial2);
-    this.flower1.position.set(0.33, 1.1, 0);
+    this.flower1.matrixAutoUpdate = false;
+    this.flower1.position.set(0.84, 1.1, 0);
     this.flower1.rotation.set(0, 0, 0.34);
     this.flower1.scale.set(1, 1, 1);
+    this.flower1.updateMatrix();
 
     this.flower2 = new THREE.Mesh(this.geometry1, this.cutiMaterial3);
-    this.flower2.position.set(0.59, -0.94, 0);
+    this.flower2.matrixAutoUpdate = false;
+    this.flower2.position.set(-0.69, -0.94, 0);
     this.flower2.rotation.set(0, 3, -0.4);
     this.flower2.scale.set(1, 1, 1);
+    this.flower2.updateMatrix();
 
     // add to a group
     this.cutiPlaneImagesTitleGroup = new THREE.Group();
-    this.cutiPlaneImagesTitleGroup.add(this.mesh1, this.flower1, this.flower2);
+    this.cutiPlaneImagesTitleGroup.add(this.flower1, this.flower2, this.lod);
+    this.cutiPlaneImagesTitleGroup.matrixAutoUpdate = false;
     this.scene.add(this.cutiPlaneImagesTitleGroup);
 
     //Debug
