f08d23c448d31790579d2e6f27d82acbe455f78a
emiliano lucero <emilianoglucero@gmail.com>
2023-10-10 12:54:43 -0300

    -cuti plane images comments deleted

--- src/Experience/World/CutiPlaneImages.js
+++ src/Experience/World/CutiPlaneImages.js
@@ -9,7 +9,6 @@ export default class CutiPlaneImages {
 
     this.amoutOfCutiImages = 10 + 1;
 
-    // this.setCutiImagePlane();
     this.setGeometry();
     this.setTextures();
     this.setMaterial();
@@ -48,18 +47,14 @@ export default class CutiPlaneImages {
         this.cutiMaterials[i]
       );
     }
-    console.log(this.cutiPlaneImages);
-    // this.cuti1 = new THREE.Mesh(this.geometry, this.cutiMaterials[0]);
     this.cutiPlaneImages[1].scale.set(1.5, 1.5, 1.5);
     this.cutiPlaneImages[1].position.set(-0.94, -0.84, -0.59);
     this.cutiPlaneImages[1].rotation.set(0.59, 0, 0);
 
-    // this.cutiPlaneImages[2] = new THREE.Mesh(this.geometry, this.cutiMaterials[1]);
     this.cutiPlaneImages[2].scale.set(1.5, 1.5, 1.5);
     this.cutiPlaneImages[2].position.set(-0.18, 0.59, -0.69);
     this.cutiPlaneImages[2].rotation.set(0.59, 0, 0);
 
-    // this.cutiPlaneImages[2] = new THREE.Mesh(this.geometry, this.cutiMaterials[2]);
     this.cutiPlaneImages[3].scale.set(1.5, 1.5, 1.5);
     this.cutiPlaneImages[3].position.set(0.46, -0.65, 0);
     this.cutiPlaneImages[3].rotation.set(5.88, 2.75, -0.34);
@@ -68,27 +63,22 @@ export default class CutiPlaneImages {
     this.cutiPlaneImages[4].position.set(1.48, 0, -0.16);
     this.cutiPlaneImages[4].rotation.set(0, -0.92, 0.13);
 
-    // this.cutiPlaneImages[5] = new THREE.Mesh(this.geometry, this.cutiMaterial5);
     this.cutiPlaneImages[5].scale.set(1.5, 1.5, 1.5);
     this.cutiPlaneImages[5].position.set(-1.63, 0.44, -0.62);
     this.cutiPlaneImages[5].rotation.set(-2.84, 0.19, 0.72);
 
-    // this.cutiPlaneImages[6] = new THREE.Mesh(this.geometry, this.cutiMaterial6);
     this.cutiPlaneImages[6].scale.set(1.5, 1.5, 1.5);
     this.cutiPlaneImages[6].position.set(0.44, 1.45, 0.93);
     this.cutiPlaneImages[6].rotation.set(0, 7.4, 0);
 
-    // this.cutiPlaneImages[7] = new THREE.Mesh(this.geometry, this.cutiMaterial7);
     this.cutiPlaneImages[7].scale.set(1.5, 1.5, 1.5);
     this.cutiPlaneImages[7].position.set(0.67, -0.42, -0.93);
     this.cutiPlaneImages[7].rotation.set(0, 3.92, 0);
 
-    // this.cutiPlaneImages[8] = new THREE.Mesh(this.geometry, this.cutiMaterial8);
     this.cutiPlaneImages[8].scale.set(1.5, 1.5, 1.5);
     this.cutiPlaneImages[8].position.set(-0.64, 1.98, -0.68);
     this.cutiPlaneImages[8].rotation.set(0, 0.65, 0.66);
 
-    // this.cutiPlaneImages[9] = new THREE.Mesh(this.geometry, this.cutiMaterial9);
     this.cutiPlaneImages[9].scale.set(1.5, 1.5, 1.5);
     this.cutiPlaneImages[9].position.set(0.78, 1.35, -0.38);
     this.cutiPlaneImages[9].rotation.set(-0.36, -0.89, 0.68);
